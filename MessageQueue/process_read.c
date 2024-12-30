20e1595d35a6b6ebdf7a855187616308ee948074
vu.nguyenchiemminh <nguyenchiemminhvu@gmail.com>
2024-12-30 22:14:45 +0700

    refactoring

---
+++ MessageQueue/process_read.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include "defs.h"
+
+int main(int argc, char** argv)
+{
+    return 0;
+}
