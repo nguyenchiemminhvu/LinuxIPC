d2adbb5e983f01009d19970858510b1616e33d5c
vu3.nguyen <vu3.nguyen@lge.com>
2024-12-30 13:59:39 +0900

    update

--- File/read_write_shared_file.c
+++ File/read_write_shared_file.c
@@ -4,9 +4,26 @@
 #include <sys/types.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/file.h>
+#include <signal.h>
 
 const char* shared_file = "/tmp/shared_file.txt";
 const int MAX_BUF = 1024;
+
+void signal_handler(int signum, siginfo_t* siginfo, void* context)
+{
+    if (signum == SIGINT)
+    {
+        printf("Cleaning up shared file...\n");
+        if (remove(shared_file) != 0)
+        {
+            perror("remove");
+        }
+        exit(0);
+    }
+}
 
 int main(int argc, char** argv)
 {
@@ -27,16 +44,43 @@ int main(int argc, char** argv)
             exit(1);
         }
 
+        int next_number = 0;
         while (1)
         {
+            struct flock lock;
+            lock.l_type = F_RDLCK; // Read lock
+            lock.l_whence = SEEK_SET; // Relative to the start of the file
+            lock.l_start = 0; // Start from the beginning of the file
+            lock.l_len = 0; // Lock the whole file
+            lock.l_pid = getpid();
+            
+            fcntl(fd, F_SETLKW, &lock);
+            if (lock.l_type != F_UNLCK)
+            {
+                char buf[MAX_BUF];
+                sprintf(buf, "%d\n", next_number);
+                write(fd, buf, strlen(buf));
+                printf("Write %d\n", next_number);
+                next_number++;
+
+                lock.l_type = F_UNLCK;
+                fcntl(fd, F_SETLK, &lock);
+            }
+
             sleep(1);
         }
 
         close(fd);
+        exit(0);
     }
     else
     {
         // Parent process (consumer)
+        struct sigaction act;
+        act.sa_sigaction = signal_handler;
+        act.sa_flags = SA_SIGINFO;
+        sigaction(SIGINT, &act, NULL);
+
         int fd = open(shared_file, O_RDWR | O_CREAT, 0666);
         if (fd < 0)
         {
@@ -46,10 +90,34 @@ int main(int argc, char** argv)
 
         while (1)
         {
+            struct flock lock;
+            lock.l_type = F_WRLCK; // Write lock
+            lock.l_whence = SEEK_SET; // Relative to the start of the file
+            lock.l_start = 0; // Start from the beginning of the file
+            lock.l_len = 0; // Lock the whole file
+            lock.l_pid = getpid();
+            
+            fcntl(fd, F_SETLKW, &lock);
+            if (lock.l_type != F_UNLCK)
+            {
+                char buf[MAX_BUF];
+                int n = read(fd, buf, MAX_BUF);
+                if (n > 0 && n < MAX_BUF)
+                {
+                    buf[n] = '\0';
+                    printf("Read %s", buf);
+                }
+
+                lock.l_type = F_UNLCK;
+                fcntl(fd, F_SETLK, &lock);
+            }
+
             sleep(1);
         }
 
         close(fd);
+        wait(NULL);
     }
+
     return 0;
 }
