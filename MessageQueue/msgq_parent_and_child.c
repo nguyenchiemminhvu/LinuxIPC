b730753e84716bfcedf5de84d5bf11edf7636e14
vu3.nguyen <vu3.nguyen@lge.com>
2024-12-31 12:19:25 +0900

    update

--- MessageQueue/msgq_parent_and_child.c
+++ MessageQueue/msgq_parent_and_child.c
@@ -6,15 +6,21 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <signal.h>
 
 #define PROJECT_ID 0x1234
 
 #define PATH_MSGQ_RANDOM "/tmp/msgq_random"
 
+struct random_msgbuf
+{
+    long mtype;
+    long mvalue;
+};
+
 key_t key_random;
 
 void init_ipc_keys() __attribute__((constructor));
-void clean_ipc_keys() __attribute__((destructor));
 
 void init_ipc_keys()
 {
@@ -36,9 +42,23 @@ void init_ipc_keys()
 
 void clean_ipc_keys()
 {
+    if (msgctl(msgget(key_random, IPC_CREAT | IPC_EXCL), IPC_RMID, NULL) == 0)
+    {
+        printf("Cleanup msgget(key_random, IPC_CREAT | IPC_EXCL)\n");
+    }
+
     if (unlink(PATH_MSGQ_RANDOM) == 0)
     {
         printf("Cleanup %s\n", PATH_MSGQ_RANDOM);
+    }
+}
+
+void signal_handler(int signum, siginfo_t *info, void *ptr)
+{
+    if (signum == SIGINT)
+    {
+        clean_ipc_keys();
+        exit(0);
     }
 }
 
@@ -60,13 +80,59 @@ int main(int argc, char** argv)
 
     if (pid == 0)
     {
-        // Child
-        
+        // Child (sender)
+        printf("CHILD: msgq_id = %d\n", msgq_id);
+
+        while (1)
+        {
+            struct random_msgbuf msgbuf;
+            msgbuf.mtype = 1;
+            msgbuf.mvalue = random();
+            if (msgsnd(msgq_id, &msgbuf, sizeof(struct random_msgbuf) - sizeof(long), 0) == -1)
+            {
+                perror("msgsnd");
+                exit(1);
+            }
+            printf("CHILD: Sent %ld\n", msgbuf.mvalue);
+
+            struct msqid_ds buf;
+            if (msgctl(msgq_id, IPC_STAT, &buf) == -1)
+            {
+                perror("msgctl IPC_STAT");
+                exit(1);
+            }
+            printf("CHILD: msg_qnum = %ld\n", buf.msg_qnum);
+
+            sleep(1);
+        }
     }
     else
     {
-        // Parent
-        
+        // Parent (receiver)
+        printf("PARENT: msgq_id = %d\n", msgq_id);
+
+        struct sigaction act;
+        memset(&act, 0, sizeof(act));
+        act.sa_sigaction = signal_handler;
+        act.sa_flags = SA_SIGINFO;
+        if (sigaction(SIGINT, &act, NULL) == -1)
+        {
+            perror("sigaction");
+            exit(1);
+        }
+
+        while (1)
+        {
+            struct random_msgbuf msgbuf;
+            if (msgrcv(msgq_id, &msgbuf, sizeof(struct random_msgbuf) - sizeof(long), 0, 0) == -1)
+            {
+                perror("msgrcv");
+                exit(1);
+            }
+            printf("PARENT: Received %ld\n", msgbuf.mvalue);
+
+            sleep(1);
+        }
 
         wait(NULL);
     }
