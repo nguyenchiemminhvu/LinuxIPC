d42672eaf0fbf7782d32f362eb6bb8d926d65779
vu3.nguyen <vu3.nguyen@lge.com>
2024-12-31 19:57:20 +0900

    refactoring

--- SharedMemory/posix_shared_mem.c
+++ SharedMemory/posix_shared_mem.c
@@ -1,12 +1,11 @@
 #include <unistd.h>
 #include <sys/types.h>
-#include <sys/ipc.h>
-#include <sys/shm.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <errno.h>
 #include <sys/wait.h>
+#include <sys/mman.h>
 
 int main(int argc, char** argv)
 {
--- SharedMemory/system_v_shared_mem.c
+++ SharedMemory/system_v_shared_mem.c
@@ -7,6 +7,7 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <sys/wait.h>
+#include <fcntl.h>
 
 int main(int argc, char** argv)
 {
