c3532b718102fa54e9c79d9424147055177cd154
vu3.nguyen <vu3.nguyen@lge.com>
2024-12-31 17:44:28 +0900

    refactoring

---
+++ SharedMemory/process_write.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/wait.h>
+
+int main(int argc, char** argv)
+{
+    return 0;
+}
