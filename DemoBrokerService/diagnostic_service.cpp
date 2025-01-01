9684db33935dfbacbc30c75ed2f80ab206b25463
vu.nguyenchiemminh <nguyenchiemminhvu@gmail.com>
2025-01-01 14:05:32 +0700

    WIP

---
+++ DemoBrokerService/diagnostic_service.cpp
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <pthread.h>
+
+int main()
+{
+    return 0;
+}
