fcdbe1f7b0bfb967ceaf4f88391cb93321fba2be
vu3.nguyen <vu3.nguyen@lge.com>
2025-01-29 00:41:34 +0900

    update

--- DBus/process_client.c
+++ DBus/process_client.c
@@ -1,6 +1,76 @@
-#include <dbus/dbus.h>  
+#include <dbus/dbus.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main()      
+void check_dbus_error(DBusError *error, const char *msg)
 {
+    if (dbus_error_is_set(error))
+    {
+        fprintf(stderr, "%s: %s\n", msg, error->message);
+        dbus_error_free(error);
+        exit(1);
+    }
+}
+
+int main()
+{
+    DBusConnection *conn;
+    DBusError error;
+    const char *input = "Hello, D-Bus!";
+    int result;
+
+    printf("Starting D-Bus client...\n");
+
+    dbus_error_init(&error);
+
+    conn = dbus_bus_get(DBUS_BUS_SESSION, &error);
+    check_dbus_error(&error, "Failed to connect to the D-Bus session bus");
+
+    while (1)
+    {
+        DBusMessage *msg, *reply;
+        DBusMessageIter args;
+
+        msg = dbus_message_new_method_call(
+            "com.example.MyService",  // Target service name
+            "/com/example/MyObject",  // Object path
+            "com.example.MyInterface", // Interface name
+            "GetStringLength"         // Method name
+        );
+
+        if (!msg)
+        {
+            fprintf(stderr, "Error: Out of memory while creating message\n");
+            exit(1);
+        }
+
+        dbus_message_iter_init_append(msg, &args);
+        if (!dbus_message_iter_append_basic(&args, DBUS_TYPE_STRING, &input))
+        {
+            fprintf(stderr, "Error: Out of memory while appending arguments\n");
+            dbus_message_unref(msg);
+            exit(1);
+        }
+
+        reply = dbus_connection_send_with_reply_and_block(conn, msg, -1, &error);
+        check_dbus_error(&error, "Failed to send message and wait for reply");
+
+        if (dbus_message_get_args(reply, &error, DBUS_TYPE_INT32, &result, DBUS_TYPE_INVALID))
+        {
+            printf("Client received reply: The length of the string is %d\n", result);
+        }
+        else
+        {
+            check_dbus_error(&error, "Failed to parse reply arguments");
+        }
+
+        dbus_message_unref(msg);
+        dbus_message_unref(reply);
+        usleep(100000);
+    }
+
+    dbus_connection_unref(conn);
+
     return 0;
 }
--- DBus/process_server.c
+++ DBus/process_server.c
@@ -1,6 +1,128 @@
-#include <dbus/dbus.h>  
+#include <dbus/dbus.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main()      
+void init_dbus_daemon(void) __attribute__((constructor));
+
+void init_dbus_daemon(void)
 {
+    char* dbus_address = getenv("DBUS_SESSION_BUS_ADDRESS");
+    if (dbus_address == NULL)
+    {
+        fprintf(stderr, "DBUS_SESSION_BUS_ADDRESS is not set. Starting a new session bus.\n");
+        system("dbus-daemon --session --fork --print-address > /tmp/dbus_address");
+        FILE* fp = fopen("/tmp/dbus_address", "r");
+        if (fp == NULL)
+        {
+            fprintf(stderr, "Failed to open /tmp/dbus_address\n");
+            exit(-1);
+        }
+        char address[256];
+        if (fgets(address, sizeof(address), fp) != NULL)
+        {
+            address[strcspn(address, "\n")] = '\0';
+            setenv("DBUS_SESSION_BUS_ADDRESS", address, 1);
+        }
+        fclose(fp);
+        setenv("DBUS_SESSION_BUS_ADDRESS", address, 1);
+        printf("DBUS_SESSION_BUS_ADDRESS=%s\n", address);
+    }
+}
+
+void check_dbus_error(DBusError *error, const char *msg)
+{
+    if (dbus_error_is_set(error))
+    {
+        fprintf(stderr, "%s: %s\n", msg, error->message);
+        dbus_error_free(error);
+        exit(1);
+    }
+}
+
+void handle_method_call(DBusMessage *msg, DBusConnection *conn)
+{
+    DBusMessage *reply;
+    DBusMessageIter args;
+    const char *input;
+    int result;
+
+    if (!dbus_message_iter_init(msg, &args) || dbus_message_iter_get_arg_type(&args) != DBUS_TYPE_STRING)
+    {
+        fprintf(stderr, "Error: Method call has no arguments or argument is not a string\n");
+        return;
+    }
+
+    dbus_message_iter_get_basic(&args, &input);
+    printf("Server received method call with input: '%s'\n", input);
+
+    result = strlen(input);
+
+    reply = dbus_message_new_method_return(msg);
+    if (!reply)
+    {
+        fprintf(stderr, "Error: Out of memory while creating reply\n");
+        return;
+    }
+
+    dbus_message_iter_init_append(reply, &args);
+    if (!dbus_message_iter_append_basic(&args, DBUS_TYPE_INT32, &result))
+    {
+        fprintf(stderr, "Error: Out of memory while appending arguments to reply\n");
+        dbus_message_unref(reply);
+        return;
+    }
+
+    if (!dbus_connection_send(conn, reply, NULL))
+    {
+        fprintf(stderr, "Error: Out of memory while sending reply\n");
+    }
+
+    dbus_connection_flush(conn);
+    dbus_message_unref(reply);
+}
+
+int main()
+{
+    DBusConnection *conn;
+    DBusError error;
+    DBusMessage *msg;
+
+    printf("Starting D-Bus server...\n");
+    dbus_error_init(&error);
+
+    conn = dbus_bus_get(DBUS_BUS_SESSION, &error);
+    check_dbus_error(&error, "Failed to connect to the D-Bus session bus");
+
+    int ret = dbus_bus_request_name(conn, "com.example.MyService", DBUS_NAME_FLAG_REPLACE_EXISTING, &error);
+    check_dbus_error(&error, "Failed to request name on the bus");
+
+    if (ret != DBUS_REQUEST_NAME_REPLY_PRIMARY_OWNER)
+    {
+        fprintf(stderr, "Error: Not the primary owner of the name\n");
+        exit(1);
+    }
+
+    printf("Server is running and waiting for method calls...\n");
+
+    while (1)
+    {
+        dbus_connection_read_write(conn, -1);
+        msg = dbus_connection_pop_message(conn);
+
+        if (msg == NULL)
+        {
+            continue;
+        }
+
+        if (dbus_message_is_method_call(msg, "com.example.MyInterface", "GetStringLength"))
+        {
+            handle_method_call(msg, conn);
+        }
+
+        dbus_message_unref(msg);
+    }
+
     return 0;
 }
